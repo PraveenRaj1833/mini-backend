ec7821c8889a6654cf41a4ed005b44d8a78cf0cf
Grocerdevteam <developer.cybercode@gmail.com>
2021-05-04 05:14:35 +0530

    Allow access updated

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','POST, OPTIONS');
+    next();
+});
+
 const url = 'mongodb+srv://Praveen:praveen123@cluster0.dav3l.mongodb.net/mini?retryWrites=true&w=majority'
 mongoose.connect(url,{useNewUrlParser:true})
 .then(()=>console.log("database connected"));
